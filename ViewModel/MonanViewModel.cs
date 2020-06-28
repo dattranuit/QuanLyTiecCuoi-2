09d2a0b7679c053e067a34b0bf408fa5ae009703
= <pvlminh3005@gmail.com>
2020-06-28 22:30:38 +0700

    finish usc_MonAn

--- ViewModel/MonanViewModel.cs
+++ ViewModel/MonanViewModel.cs
@@ -5,6 +5,8 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
 using System.Windows.Input;
 using QuanLyTiecCuoi.Model;
 
@@ -36,6 +38,106 @@ namespace QuanLyTiecCuoi.ViewModel
             }
         }
 
+        //public event PropertyChangedEventHandler PropertyChanged;
+        //private void NotifyPropertyChanged(string property)
+        //{
+        //    if (PropertyChanged != null)
+        //    {
+        //        PropertyChanged(this, new PropertyChangedEventArgs(property));
+        //    }
+        //}
+        //biến bên trang chính
+        private int _MaMonAn { get; set; }
+        public int MaMonAn { get => _MaMonAn; set { _MaMonAn = value; OnPropertyChanged(); } }
+        private string _TenMonAn { get; set; }
+        public string TenMonAn { get => _TenMonAn; set { _TenMonAn = value; OnPropertyChanged(); } }
+        private decimal _DonGia { get; set; }
+        public decimal DonGia { get => _DonGia; set { _DonGia = value; OnPropertyChanged(); } }
+        private string _MoTa { get; set; }
+        public string MoTa { get => _MoTa; set { _MoTa = value; OnPropertyChanged(); } }
+        public string HinhAnh { get; set; }
+        private string _GhiChu { get; set; }
+        public string GhiChu { get => _GhiChu; set { _GhiChu = value; OnPropertyChanged(); } }
+        private int _MaLoaiMonAn { get; set; }
+        public int MaLoaiMonAn { get => _MaLoaiMonAn; set { _MaLoaiMonAn = value; OnPropertyChanged(); } }
+
+        public ICommand AddCommand { get; set; }
+        public ICommand EditCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        public ICommand ClickCommand { get; set; }
+
+
+        public MonanViewModel()
+        {
+            List = new ObservableCollection<MONAN>(DataProvider.Ins.DataBase.MONANs);
+            DataGridCollection = CollectionViewSource.GetDefaultView(List);
+            DataGridCollection.Filter = new Predicate<object>(Filter);
+            AddCommand = new RelayCommand<object>((p) =>
+            {
+                //if (string.IsNullOrEmpty(TenMonAn))
+                //    return false;
+                //var displayList = DataProvider.Ins.DataBase.MONANs.Where(x => x.TenMonAn == TenMonAn);
+                //if (displayList == null && displayList.Count() != 0)
+                //    return false;
+                return true;
+
+            }, (p) =>
+            {
+                var MonAn = new MONAN()
+                {
+                    MaMonAn = MaMonAn,
+                    TenMonAn = TenMonAn,
+                    DonGia = DonGia,
+                    MoTa = MoTa,
+                    GhiChu = GhiChu
+                };
+                DataProvider.Ins.DataBase.MONANs.Add(MonAn);
+                DataProvider.Ins.DataBase.SaveChanges();
+                List.Add(MonAn);
+            });
+
+            EditCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(TenMonAn) || SelectedItem == null)
+                    return false;
+                var displayList = DataProvider.Ins.DataBase.MONANs.Where(x => x.TenMonAn == TenMonAn);
+                if (displayList == null && displayList.Count() != 0)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var MonAn = DataProvider.Ins.DataBase.MONANs.Where(x => x.MaMonAn == SelectedItem.MaMonAn).SingleOrDefault();
+                //MonAn.MaMonAn = SelectedItem.MaMonAn;
+                //MonAn.TenMonAn = SelectedItem.TenMonAn;
+                //MonAn.DonGia = SelectedItem.DonGia;
+                //MonAn.MoTa = SelectedItem.MoTa;
+                //MonAn.GhiChu = SelectedItem.GhiChu;
+                DataProvider.Ins.DataBase.SaveChanges();
+
+                SelectedItem.TenMonAn = TenMonAn;
+                SelectedItem.MaMonAn = MaMonAn;
+                SelectedItem.DonGia = DonGia;
+                SelectedItem.MoTa = MoTa;
+                SelectedItem.GhiChu = GhiChu;
+            });
+            DeleteCommand = new RelayCommand<object>((p) =>
+            {
+                if (string.IsNullOrEmpty(TenMonAn) || SelectedItem == null)
+                    return false;
+                var displayList = DataProvider.Ins.DataBase.MONANs.Where(x => x.TenMonAn == SelectedItem.TenMonAn);
+                if (displayList == null && displayList.Count() != 0)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var MonAn = DataProvider.Ins.DataBase.MONANs.Where(x => x.MaMonAn == SelectedItem.MaMonAn).SingleOrDefault();
+                DataProvider.Ins.DataBase.MONANs.Remove(MonAn);
+                DataProvider.Ins.DataBase.SaveChanges();
+                MessageBox.Show("Xóa thành công!");
+            });
+        }
+
+        // Search DataGrid
         private ICollectionView _dataGridCollection;
         private string _filterString;
         public ICollectionView DataGridCollection
@@ -72,95 +174,6 @@ namespace QuanLyTiecCuoi.ViewModel
                 return true;
             }
             return false;
-        }
-        public event PropertyChangedEventHandler PropertyChanged;
-        private void NotifyPropertyChanged(string property)
-        {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(property));
-            }
-        }
-        //biến bên trang chính
-        private int _MaMonAn { get; set; }
-        public int MaMonAn { get => _MaMonAn; set { _MaMonAn = value; OnPropertyChanged(); } }
-        private string _TenMonAn { get; set; }
-        public string TenMonAn { get => _TenMonAn; set { _TenMonAn = value; OnPropertyChanged(); } }
-        private decimal _DonGia { get; set; }
-        public decimal DonGia { get => _DonGia; set { _DonGia = value; OnPropertyChanged(); } }
-        private string _MoTa { get; set; }
-        public string MoTa { get => _MoTa; set { _MoTa = value; OnPropertyChanged(); } }
-        public string HinhAnh { get; set; }
-        private string _GhiChu { get; set; }
-        public string GhiChu { get => _GhiChu; set { _GhiChu = value; OnPropertyChanged(); } }
-        private int _MaLoaiMonAn { get; set; }
-        public int MaLoaiMonAn { get => _MaLoaiMonAn; set { _MaLoaiMonAn = value; OnPropertyChanged(); } }
-
-        public ICommand AddCommand { get; set; }
-        public ICommand EditCommand { get; set; }
-        public ICommand DeleteCommand { get; set; }
-        public ICommand ClickCommand { get; set; }
-
-
-        public MonanViewModel()
-        {
-            List = new ObservableCollection<MONAN>(DataProvider.Ins.DataBase.MONANs);
-            AddCommand = new RelayCommand<object>((p) =>
-            {
-                //if (string.IsNullOrEmpty(TenMonAn))
-                //    return false;
-                //var displayList = DataProvider.Ins.DataBase.MONANs.Where(x => x.TenMonAn == TenMonAn);
-                //if (displayList == null && displayList.Count() != 0)
-                //    return false;
-                return true;
-
-            }, (p) =>
-            {
-                var MonAn = new MONAN()
-                {
-                    MaMonAn = MaMonAn,
-                    TenMonAn = TenMonAn,
-                    DonGia = DonGia,
-                    MoTa = MoTa,
-                    GhiChu = GhiChu
-                };
-                DataProvider.Ins.DataBase.MONANs.Add(MonAn);
-                DataProvider.Ins.DataBase.SaveChanges();
-                List.Add(MonAn);
-            });
-
-            EditCommand = new RelayCommand<object>((p) =>
-            {
-                if (string.IsNullOrEmpty(TenMonAn) || SelectedItem == null)
-                    return false;
-                var displayList = DataProvider.Ins.DataBase.MONANs.Where(x => x.TenMonAn == SelectedItem.TenMonAn);
-                if (displayList == null && displayList.Count() != 0)
-                    return false;
-                return true;
-            }, (p) =>
-            {
-                var MonAn = DataProvider.Ins.DataBase.MONANs.Where(x => x.MaMonAn == SelectedItem.MaMonAn).SingleOrDefault();
-                MonAn.MaMonAn = SelectedItem.MaMonAn;
-                MonAn.TenMonAn = SelectedItem.TenMonAn;
-                MonAn.DonGia = SelectedItem.DonGia;
-                MonAn.MoTa = SelectedItem.MoTa;
-                MonAn.GhiChu = SelectedItem.GhiChu;
-                DataProvider.Ins.DataBase.SaveChanges();
-
-                SelectedItem.TenMonAn = TenMonAn;
-                SelectedItem.MaMonAn = MaMonAn;
-                SelectedItem.DonGia = DonGia;
-                SelectedItem.MoTa = MoTa;
-                SelectedItem.GhiChu = GhiChu;
-            });
-            //DeleteCommand = new RelayCommand<object>((p) =>
-            //{
-
-
-
-            //});
-
-
         }
 
     }
