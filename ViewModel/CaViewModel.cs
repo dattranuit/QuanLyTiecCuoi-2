bd997b0166f8fbd0679b52dafdd22361942c312a
woanebk@gmail.com <woanebk@gmail.com>
2020-07-02 23:46:35 +0700

    Quy dinh: THEM, XOA, Sua Ca ok. Nhưng timepicker chưa binding.

--- ViewModel/CaViewModel.cs
+++ ViewModel/CaViewModel.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using QuanLyTiecCuoi.Model;
 using System.Windows.Input;
 using System.Collections.ObjectModel;
+using System.Windows;
 
 namespace QuanLyTiecCuoi.ViewModel
 {
@@ -31,11 +32,16 @@ namespace QuanLyTiecCuoi.ViewModel
         }
         private string _TenCa;
         public string TenCa { get => _TenCa; set { _TenCa = value; OnPropertyChanged(); } }
+        private System.TimeSpan _BatDau;
+        public System.TimeSpan BatDau { get => _BatDau; set { _BatDau = value; OnPropertyChanged(); } }
+        private System.TimeSpan _KetThuc;
+        public System.TimeSpan KetThuc { get => _KetThuc; set { _KetThuc = value; OnPropertyChanged(); } }
         private int _MaCa;
         public int MaCa { get => _MaCa; set { _MaCa = value; OnPropertyChanged(); } }
         public ICommand AddCommand { get; set; }
         public ICommand EditCommand { get; set; }
-        public ICommand PhieuDatBanCommand { get; set; }
+        public ICommand DeleteCommand { get; set; }
+        
         public CaViewModel()
         {
             ListCa = new ObservableCollection<CA>(DataProvider.Ins.DataBase.CAs);
@@ -47,11 +53,15 @@ namespace QuanLyTiecCuoi.ViewModel
                 var Ca = new CA()
                 {
                     TenCa = TenCa,
-                    MaCa = MaCa                    
+                    MaCa = MaCa,
+                    BatDau=BatDau,
+                    KetThuc=KetThuc,
                 };
                 DataProvider.Ins.DataBase.CAs.Add(Ca);
                 DataProvider.Ins.DataBase.SaveChanges();
                 ListCa.Add(Ca);
+                MessageBox.Show("Thêm Ca thành công !");
+                SelectedItem = Ca;
             });
 
             EditCommand = new RelayCommand<object>((p) =>
@@ -59,15 +69,41 @@ namespace QuanLyTiecCuoi.ViewModel
                 if (SelectedItem == null)
                     return false;
                 var displayList = DataProvider.Ins.DataBase.CAs.Where(x => x.MaCa == SelectedItem.MaCa);
-                if (displayList != null && displayList.Count() != 0)
-                    return true;
-                return false;
+                if (displayList == null && displayList.Count() == 0)
+                    return false;
+                if (SelectedItem.TenCa == TenCa && SelectedItem.BatDau == BatDau && SelectedItem.KetThuc == KetThuc)
+                    return false;
+                return true;
             }, (p) =>
             {
                 var Ca = DataProvider.Ins.DataBase.CAs.Where(x => x.MaCa == SelectedItem.MaCa).SingleOrDefault();
                 Ca.TenCa = SelectedItem.TenCa;
-                Ca.MaCa = SelectedItem.MaCa;
+                Ca.BatDau = SelectedItem.BatDau;
+                Ca.KetThuc = SelectedItem.KetThuc;
                 DataProvider.Ins.DataBase.SaveChanges();
+                MessageBox.Show("Sửa thông tin Ca thành công !");
+            });
+
+            DeleteCommand = new RelayCommand<object>((p) =>
+            {
+                if (SelectedItem == null)
+                    return false;
+                return true;
+            }, (p) =>
+            {
+                var Ca = DataProvider.Ins.DataBase.CAs.Where(x => x.MaCa == SelectedItem.MaCa).First();
+                var TiecCuoi = DataProvider.Ins.DataBase.TIECCUOIs.Where(x => x.MaCa == SelectedItem.MaCa);
+                if (TiecCuoi.Count() != 0)
+                {
+                    MessageBox.Show("Không thể xóa vì có tồn tại Tiệc Cưới được đặt ở Ca này !");
+                    return;
+                }
+                DataProvider.Ins.DataBase.CAs.Remove(Ca);
+                DataProvider.Ins.DataBase.SaveChanges();
+                ListCa.Remove(Ca);
+                MessageBox.Show("Xóa Ca thành công !");
+                //refresh nhap
+                TenCa = "";
             });
         }
     }
