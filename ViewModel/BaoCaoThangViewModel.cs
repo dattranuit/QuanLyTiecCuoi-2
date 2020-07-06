64d169897d1f8a6acf7b4d10eef0e9b4261b1811
dattranuit <18520590@gm.uit.edu.vn>
2020-07-06 20:02:29 +0700

    Finish?

--- ViewModel/BaoCaoThangViewModel.cs
+++ ViewModel/BaoCaoThangViewModel.cs
@@ -134,27 +134,36 @@ namespace QuanLyTiecCuoi.ViewModel
         // Export to excel
         private void Ex2Excel(DataGrid dataGrid)
         {
-            Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
-            excel.Visible = true;
-            Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
-            Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
+            try
+            {
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                excel.Visible = true;
+                Workbook workbook = excel.Workbooks.Add(System.Reflection.Missing.Value);
+                Worksheet sheet1 = (Worksheet)workbook.Sheets[1];
 
-            for (int j = 0; j < dataGrid.Columns.Count; j++)
-            {
-                Range myRange = (Range)sheet1.Cells[1, j + 1];
-                sheet1.Cells[1, j + 1].Font.Bold = true;
-                sheet1.Columns[j + 1].ColumnWidth = 15;
-                myRange.Value2 = dataGrid.Columns[j].Header;
-            }
-            for (int i = 0; i < dataGrid.Columns.Count; i++)
-            {
-                for (int j = 0; j < dataGrid.Items.Count; j++)
+                for (int j = 0; j < dataGrid.Columns.Count; j++)
                 {
-                    TextBlock b = dataGrid.Columns[i].GetCellContent(dataGrid.Items[j]) as TextBlock;
-                    Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
-                    myRange.Value2 = b.Text;
+                    Range myRange = (Range)sheet1.Cells[1, j + 1];
+                    sheet1.Cells[1, j + 1].Font.Bold = true;
+                    sheet1.Columns[j + 1].ColumnWidth = 15;
+                    myRange.Value2 = dataGrid.Columns[j].Header;
                 }
+                for (int i = 0; i < dataGrid.Columns.Count; i++)
+                {
+                    for (int j = 0; j < dataGrid.Items.Count; j++)
+                    {
+                        TextBlock b = dataGrid.Columns[i].GetCellContent(dataGrid.Items[j]) as TextBlock;
+                        Microsoft.Office.Interop.Excel.Range myRange = (Microsoft.Office.Interop.Excel.Range)sheet1.Cells[j + 2, i + 1];
+                        myRange.Value2 = b.Text;
+                    }
+                }
+
             }
+            catch(Exception e)
+            {
+                MessageBox.Show("Không thể xuất file excel!" + e.ToString());
+            }
+
 
         }
 
